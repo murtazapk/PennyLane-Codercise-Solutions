c2ec67ea23f11cfe32e188e9f3551a16148e2b8a
murtazapk <engrgm96@gmail.com>
2026-02-02 13:40:16 +0000

    Implement state normalization function
    Added a function to normalize quantum states.

---
+++ Introduction to Quantum Computing/I.1.1-Codercise.py
@@ -0,0 +1,10 @@
+#codercise 1.1.1
+ket_0=np.array([1,0])
+ket_1=np.array([0,1])
+def normalize_state(alpha,beta):
+    unnormalized_state=np.array([alpha,beta], dtype=np.complex128)
+    norm=np.linalg.norm(unnormalized_state)
+    normalized_state=unnormalized_state/norm
+
+    return normalized_state
+    pass
