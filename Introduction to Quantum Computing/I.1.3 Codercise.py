71c6c4a418b47c11379c46931421a369e080b7e3
Murtaza <engrgm96@gmail.com>
2026-02-02 15:23:51 +0000

    Import PennyLane and NumPy for quantum measurements
    Added import statements for PennyLane and NumPy.

--- Introduction to Quantum Computing/I.1.3 Codercise.py
+++ Introduction to Quantum Computing/I.1.3 Codercise.py
@@ -1,4 +1,7 @@
 # I.1.3 Sampling measurement outcome
+
+import pennylane as qml
+from pennylane import numpy as np
 def measure_state(state,num_meas):
     probabilities=np.abs(state)**2
     samples=np.random.choice(
